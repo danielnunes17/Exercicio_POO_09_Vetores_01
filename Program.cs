0b18c23402571569f77db2a4cf8a114cb5c8dede
Daniel Stephanio Nunes <dti584@localiza.com>
2023-03-01 10:52:38 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Exercicio_POO_09_Vetores_01;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Estudante[] vetorEstudante = new Estudante[10];
+
+        Console.WriteLine("Quantos quartos serão alugados: ");
+        int quantidadeQuartos = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= quantidadeQuartos; i++)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Aluguel #{i}:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("E-mail: ");
+            string email = Console.ReadLine();
+            Console.Write("Quarto: ");
+            int quarto = int.Parse(Console.ReadLine());
+            vetorEstudante[quarto] = new Estudante(nome, email);
+        }
+        Console.WriteLine();
+        Console.WriteLine("Quartos ocupados: ");
+        for (int i = 0; i < 10; i++)
+        {
+            if (vetorEstudante[i] != null)
+                Console.WriteLine(i + ", " + vetorEstudante[i]);
+        }
+    }
+}
